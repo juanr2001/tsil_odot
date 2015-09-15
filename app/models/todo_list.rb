2c8ed78713a3ef192a2ed8b5240a2eb2ec66d1fb
Juan M. Ordaz Velazquez <juanordaz_2011@icloud.com>
2015-09-15 17:31:12 -0400

    Add validations for todo list description to make my test to pass

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -3,5 +3,7 @@ class TodoList < ActiveRecord::Base
     #Validations
     validates :title, presence: true
     validates :title, length: {minimum: 3}
+    validates :description, presence: true
+    validates :description, length: {minimum: 5}
 
 end
