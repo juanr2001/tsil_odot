5aa3a3a60067079cacd6d26ba87424e08436e31c
Juan M. Ordaz Velazquez <juanordaz_2011@icloud.com>
2015-09-16 12:09:21 -0400

    Add a link to test views

--- spec/features/todo_items/index_spec.rb
+++ spec/features/todo_items/index_spec.rb
@@ -2,13 +2,14 @@ require 'rails_helper'
 
 RSpec.feature "Todo List", :type => :feature do
 
-    let!(:todo_list) {TodoList.create(title: "Homework", description: "Math test coming up") }
     describe "Viewing todo items" do
+        let!(:todo_list) {TodoList.create(title: "Homework", description: "Math test coming up") }
         it "displays no items when a todo list is empty" do
             visit "/todo_lists"
             within "#todo_list_#{todo_list.id}" do
                 click_link "List Items"
+            end
+        expect(page).to have_content("TodoItems#index")
         end
-        expect(page).to have_content("TodoItem#index")
     end
 end
