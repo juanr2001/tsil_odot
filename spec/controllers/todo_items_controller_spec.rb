395118fa5bea5ca60b849f8c99ae284e672dbb54
Juan M. Ordaz Velazquez <juanordaz_2011@icloud.com>
2015-09-17 09:32:14 -0400

    Comment the test generated, I am using capybara

--- spec/controllers/todo_items_controller_spec.rb
+++ spec/controllers/todo_items_controller_spec.rb
@@ -2,11 +2,13 @@ require 'rails_helper'
 
 RSpec.describe TodoItemsController, type: :controller do
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
+#I commented this out, because I am using capybara for testing.
+
+  # describe "GET #index" do
+  #   it "returns http success" do
+  #     get :index
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
 end
