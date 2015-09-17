bf34d1e207f5002bb4eb4fccb09ecfb6a6835cc6
Juan M. Ordaz Velazquez <juanordaz_2011@icloud.com>
2015-09-17 15:35:53 -0400

    Added a complete method

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -10,45 +10,52 @@ class TodoItemsController < ApplicationController
     end
 
     def create
-        @todo_item = @todo_list.todo_items.new(todo_item_params)
+        @todo_item = @todo_list.todo_items.new( todo_item_params )
 
         if @todo_item.save
-            flash[:success] = "Added todo list item."
+            flash[ :success ] = "Added todo list item."
             redirect_to todo_list_todo_items_path
 
         else
-            flash[:error] = "There was a problem adding that todo item"
+            flash[ :error ] = "There was a problem adding that todo item"
             render action: :new
         end
     end
 
     def edit
                                                                     #Fetch it with the 'id' bacause I am editing, not creating a new one.
-        @todo_item = @todo_list.todo_items.find(params[:id])
+        @todo_item = @todo_list.todo_items.find( params[ :id ] )
     end
 
     def update
         @todo_list = TodoList.find( params[ :todo_list_id ] )
-        @todo_item = @todo_list.todo_items.find(params[:id])
+        @todo_item = @todo_list.todo_items.find( params[ :id ] )
 
         if @todo_item.update_attributes(todo_item_params)
-            flash[:success] = "Saved todo list item."
+            flash[ :success ] = "Saved todo list item."
             redirect_to todo_list_todo_items_path
 
         else
-            flash[:error] = "There was a problem adding that todo item"
+            flash[ :error ] = "There was a problem adding that todo item"
             render action: :new
         end
     end
 
     def destroy
         @todo_item = @todo_list .todo_items.find( params[ :id ] )
-        if @todo_item.destroy 
-            flash[:success] = "Todo list item was deleted."
-        else 
-            flash[:error] = "Todo list item could not be deleted."
+        if @todo_item.destroy
+            flash[ :success ] = "Todo list item was deleted."
+        else
+            flash[ :error ] = "Todo list item could not be deleted."
         end
         redirect_to todo_list_todo_items_path
+    end
+
+    def complete
+        @todo_item = @todo_list.todo_items.find( params[ :id ] )
+        #Time.now, will create a time of that exact moment when I press the button
+        @todo_item.update_attribute( :completed_at, Time.now )
+        redirect_to todo_list_todo_items_path, notice: "Todo item marked as complete."
     end
 
 #This method will prevent me to write (@todo_list_id) in the controller and the view every time
@@ -57,7 +64,7 @@ class TodoItemsController < ApplicationController
 
     def url_options
                                                                 #Everytime it generates a URL this will be call.
-        { todo_list_id: params[:todo_list_id] }.merge(super)
+        { todo_list_id: params[ :todo_list_id ] }.merge( super )
     end
 
     private
@@ -67,7 +74,7 @@ class TodoItemsController < ApplicationController
     end
 
     def todo_item_params
-        params[:todo_item].permit(:content)
+        params[ :todo_item ].permit( :content )
     end
 
 end
