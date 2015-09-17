fc58c3e5ff69cb00c48aab8dd31a3704ea151466
Juan M. Ordaz Velazquez <juanordaz_2011@icloud.com>
2015-09-17 09:33:07 -0400

    Wrote a test when creating a item

--- spec/features/todo_items/create_spec.rb
+++ spec/features/todo_items/create_spec.rb
@@ -21,5 +21,29 @@ RSpec.feature "Todo List", :type => :feature do
                 expect(page).to have_content("Milk")
             end
         end
+
+        it "displays an error with no content" do
+            visit_todo_list(todo_list)
+            click_link "New Todo Item"
+            fill_in "Content", with: ""
+            click_button "Save"
+            #checking the flash message from the controller.
+            within("div.flash") do
+                expect(page).to have_content("There was a problem adding that todo item")
+            end
+            expect(page).to have_content("Content can't be blank")
+        end
+
+        it "displays an error with content less than 2 characters long" do
+            visit_todo_list(todo_list)
+            click_link "New Todo Item"
+            fill_in "Content", with: "H"
+            click_button "Save"
+            #checking the flash message from the controller.
+            within("div.flash") do
+                expect(page).to have_content("There was a problem adding that todo item")
+            end
+            expect(page).to have_content("Content is too short")
+        end
     end
 end
