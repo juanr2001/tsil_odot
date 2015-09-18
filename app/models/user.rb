1faf8b053b641be0c572931e24f32b27c17e8cdf
Juan M. Ordaz Velazquez <juanordaz_2011@icloud.com>
2015-09-18 12:57:06 -0400

    added a callback and method to downcase email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+
+    #validations
+    validates :email, presence: true,
+                                                uniqueness: true
+
+    #callbacks to the models
+    before_save( :downcase_email )
+
+    def downcase_email
+        self.email = email.downcase
+    end
+
+
 end
