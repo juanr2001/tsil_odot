2cb3836d24224875303a1bafb5d1646b36307eb5
Juan M. Ordaz Velazquez <juanordaz_2011@icloud.com>
2015-09-18 10:53:42 -0400

    Add scopes

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -3,6 +3,13 @@ class TodoItem < ActiveRecord::Base
     validates :content, presence: true,
                                                     length: { minimum: 2 }
 
+
+    #I want to check when Items are complete or incomplete
+    #So Whe I say todo_items.complete, it will look like this is the database
+    #These are methods called that allows me to create a method for commonly used queries on models.
+    scope :complete, -> { where( "completed_at is not null" ) } # in the database
+    scope :incomplete, -> { where( completed_at: nil ) } #rails write this for me
+
     def completed?
         !completed_at.blank?
     end
