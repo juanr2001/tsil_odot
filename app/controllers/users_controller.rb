9aca818f429e7629cc488bb8fbbff674d11e18c8
Juan M. Ordaz Velazquez <juanordaz_2011@icloud.com>
2015-09-18 12:07:21 -0400

    delete index and show methods, i don't need them

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:edit, :update, :destroy]
 
-  # GET /users
-  # GET /users.json
-  def index
-    @users = User.all
-  end
 
-  # GET /users/1
-  # GET /users/1.json
-  def show
-  end
 
   # GET /users/new
   def new
