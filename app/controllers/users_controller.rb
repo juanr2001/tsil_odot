ff93eaff51914f4cbceb54f1685cd0afa7b42bc0
Juan M. Ordaz Velazquez <juanordaz_2011@icloud.com>
2015-09-18 13:45:11 -0400

    Changed the params inside the strong params method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -60,6 +60,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password_digest)
+      #I have to change passwor.digest, because i am passing password and password confirmation instead
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
     end
 end
