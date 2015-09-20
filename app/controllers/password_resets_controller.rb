78b757b0356c6c045688ceb266c22c892c978891
Juan M. Ordaz Velazquez <juanordaz_2011@icloud.com>
2015-09-20 13:20:54 -0400

    Added flashes and conditional

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -3,11 +3,16 @@ class PasswordResetsController < ApplicationController
     def new
     end
     def create
-        #this code is to just make my test to pass for now
         user = User.find_by(email: params[ :email ] )
-        user.generate_password_reset_token!
-        Notifier.password_reset(user).deliver_now
-        redirect_to login_path
+        if user
+            user.generate_password_reset_token!
+            Notifier.password_reset(user).deliver_now
+            flash[ :success ] = "Password reset instructions sent! Please check your email."
+            redirect_to login_path
+        else
+            flash.now[ :notice ] = "Email not found."
+            render action: 'new'
+        end
     end
 
 end
