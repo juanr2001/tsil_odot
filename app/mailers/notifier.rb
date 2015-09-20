dbbfe1e188e3b97e904a8b93077151d355336fbf
Juan M. Ordaz Velazquez <juanordaz_2011@icloud.com>
2015-09-20 12:22:15 -0400

    added the method to fetch the user

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,2 +1,12 @@
 class Notifier < ApplicationMailer
+
+    #this is needed, bc an email does not what host is from.
+    default_url_options[ :host ] = "localhost:3000"
+    default from: "no-reply@example.com"
+
+    def password_reset(user)
+        @user = user
+        mail( to: "#{user.first_name} #{user.last_name} <#{user.email}>",
+                subject: "Reset Your Password" )
+    end
 end
