d3a345a01cbd898fa8614963734f31d6de18eecf
Juan M. Ordaz Velazquez <juanordaz_2011@icloud.com>
2015-09-21 16:58:40 -0400

    changed the element tags, since I updated in the views

--- spec/features/todo_items/create_spec.rb
+++ spec/features/todo_items/create_spec.rb
@@ -17,7 +17,7 @@ RSpec.feature "Todo List", :type => :feature do
             fill_in "Content", with: "Milk"
             click_button "Save"
             expect(page).to have_content("Added todo list item.")
-            within("table.todo_items") do
+            within(".todo-items") do
                 expect(page).to have_content("Milk")
             end
         end
