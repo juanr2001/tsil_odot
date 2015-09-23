2901653155068c29091e3ef221e60ebafdb38d4d
Juan M. Ordaz Velazquez <juanordaz_2011@icloud.com>
2015-09-23 10:13:59 -0400

    modified title method to add content_tag helper to DRY up h1 tags in my pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,14 @@ module ApplicationHelper
 
     def title(title)
         content_for( :title ) {"#{title} | "}
+        #This is the same as >>  <h2 class = "page-title truncate" title = "<%= @todo_list.title %>"><%= @todo_list.title%></h2>
+        # same as >> <h1><%= @todo_list.title %> - Editing Todo List Item</h1>
+        #same as >> <h2 class = "page-title">Todo Lists</h2>
+        #same as >> <h1>Editing Todo List</h1>
+        #same as >> <h1>New Todo List</h1>
+        #this is call in todo_items/index, edit, new,
+        #This is call in todo_list/index,show, new, edit
+        content_tag( :h2, title, class: "page-title truncate", title: title )
     end
 
     def new_item_link
@@ -13,4 +21,5 @@ module ApplicationHelper
         end
         link_to "Add #{text}", path, class: "icon-new right hide-text"
     end
+
 end
