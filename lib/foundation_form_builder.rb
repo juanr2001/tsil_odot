769e7c92e6c5bef214b06a1ddc151862ad19e129
Juan M. Ordaz Velazquez <juanordaz_2011@icloud.com>
2015-09-23 18:16:01 -0400

    created the divs submit text_field, label.

--- lib/foundation_form_builder.rb
+++ lib/foundation_form_builder.rb
@@ -8,4 +8,45 @@ class FoundationFormBuilder < ActionView::Helpers::FormBuilder
 
     attr_accessor :output_buffer
 
+   # <div class = "row">
+   #    <div class = "small-12 columns">
+   #      <%= f.text_field :title %>
+   #    </div>
+   #  </div>
+
+   def text_field( attribute, options = {} )
+    options[ :lable ] ||= attribute
+    #Now I can pass a string with "Now I Have a String"
+    label_text ||= options.delete( :label ).to_s.titleize
+        wrapper do
+        # content_tag(:div, class: "row" ) do
+        #     content_tag( :div, class: "small-12 columns") do
+                #label
+                label( attribute, label_text ) +
+                #title, super calls ActionView Helpers text_field
+                super( attribute, options )
+        #     end
+        # end
+        end
+   end
+
+   def submit( text, options = {} )
+        options[ :class ] ||= "button radius expand"
+        wrapper do
+        # content_tag( :div, class: "row" ) do
+        #     content_tag( :div, class: "small-12 columns" ) do
+                super(text, options)
+            # end
+        # end
+        end
+   end
+
+   def wrapper( options = {}, &block)
+        content_tag( :div, class: "row") do
+            #by default content tag allows to pass text inside a div content_tag( :div, text, class: "small-12 columns")
+            #using Capture Helper I can pass a block of text instead. So It allows me to pass super(text, label_text) inside the block
+            content_tag( :div, capture(&block), class: "small-12 columns")
+        end
+   end
+
 end
