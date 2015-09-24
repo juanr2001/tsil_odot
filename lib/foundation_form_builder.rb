bea542a6642be4ed7456d9251780f75d56da4f27
Juan M. Ordaz Velazquez <juanordaz_2011@icloud.com>
2015-09-24 10:12:09 -0400

    Modify my code to display the write error class

--- lib/foundation_form_builder.rb
+++ lib/foundation_form_builder.rb
@@ -8,20 +8,6 @@ class FoundationFormBuilder < ActionView::Helpers::FormBuilder
 
     attr_accessor :output_buffer
 
-   def text_field( attribute, options = {} )
-    options[ :lable ] ||= attribute
-    label_text ||= options.delete( :label ).to_s.titleize
-    label_options ||= {}
-    if errors_on?(attribute)
-        # label_options[ :class ] = "error"
-        # options[ :class ] = "error"
-        wrapper_options = { wrapper_classes: "error"}
-    end
-    wrapper( wrapper_options ) do
-        label( attribute, label_text, label_options ) +
-        super( attribute, options ) + errors_for_field(attribute)
-    end
-   end
 
     def errors_on?( attribute )
         object.errors[ attribute ].size > 0
@@ -42,9 +28,19 @@ class FoundationFormBuilder < ActionView::Helpers::FormBuilder
 
    def errors_for_field( attribute, options = {} )
     return "" if object.errors[ attribute ].empty?
-    #since form_for calls an object, I can call the word object here. because is inside the form_for block
-    #so the errors are call from the model validations, depending on the model and the attribute.
     content_tag( :small, object.errors[ attribute ].to_sentence.capitalize, class: "error" )
    end
 
+   def text_field( attribute, options = {} )
+    options[ :lable ] ||= attribute
+    label_text ||= options.delete( :label ).to_s.titleize
+    label_options ||= {}
+    if errors_on?(attribute)
+        wrapper_options = { wrapper_classes: "error"}
+    end
+    wrapper( wrapper_options ) do
+        label( attribute, label_text, label_options ) +
+        super( attribute, options ) + errors_for_field(attribute)
+    end
+   end
 end
